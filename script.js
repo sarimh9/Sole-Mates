dba922d389b8c43f4ba1cec055f72bf351fc3f62
Sarim Hussain <sarimh9@gmail.com>
2022-03-24 12:14:10 +0530

    Minor Bugs removed

--- script.js
+++ script.js
@@ -187,3 +187,84 @@ const closeButton = document.querySelector(".close");
 closeButton.addEventListener("click", () => {
   paymentModal.classList.add("displayNone");
 });
+
+const features = document.querySelector(".features");
+features.innerHTML += `
+<div class="feature">
+      <img class="featureIcon" src="./images/shipping.png" alt="">
+      <span class="featureTitle">FREE SHIPPING</span>
+      <span class="featureDescription">Free world wide shipping on all orders.</span>
+    </div>
+    <div class="feature">
+      <img class="featureIcon" src="./images/return.png" alt="">
+      <span class="featureTitle">30 DAYS RETURN</span>
+      <span class="featureDescription">No question return and easy refund in 14 days.</span>
+    </div>
+    <div class="feature">
+      <img class="featureIcon" src="./images/gift.png" alt="">
+      <span class="featureTitle">GIFT CARDS</span>
+      <span class="featureDescription">Buy gift cards and use coupon code easily.</span>
+    </div>
+    <div class="feature">
+      <img class="featureIcon" src="./images/contact.png" alt="">
+      <span class="featureTitle">CONTACT US!</span>
+      <span class="featureDescription">Keep in touch via e-mail and our support system</span>
+    </div>
+`;
+
+const footer = document.querySelector(".footer");
+footer.innerHTML += `
+<div class="footerLeft">
+<div class="footerMenu">
+  <h1 class="fMenuTitle">About Us</h1>
+  <ul class="fList">
+    <li class="fListItem">Company</li>
+    <li class="fListItem">Contact</li>
+    <li class="fListItem">Careers</li>
+    <li class="fListItem">Affiliates</li>
+    <li class="fListItem">Stores</li>
+  </ul>
+</div>
+<div class="footerMenu">
+  <h1 class="fMenuTitle">Useful Links</h1>
+  <ul class="fList">
+    <li class="fListItem">Support</li>
+    <li class="fListItem">Refund</li>
+    <li class="fListItem">FAQ</li>
+    <li class="fListItem">Feedback</li>
+    <li class="fListItem">Stories</li>
+  </ul>
+</div>
+<div class="footerMenu">
+  <h1 class="fMenuTitle">Products</h1>
+  <ul class="fList">
+    <li class="fListItem">Air Force</li>
+    <li class="fListItem">Air Jordan</li>
+    <li class="fListItem">Blazer</li>
+    <li class="fListItem">Crater</li>
+    <li class="fListItem">Hippie</li>
+  </ul>
+</div>
+</div>
+<div class="footerRight">
+<div class="footerRightMenu">
+  <h1 class="fMenuTitle">Subscribe to our newsletter</h1>
+  <div class="fMail">
+    <input type="email" placeholder="your@email.com" class="fInput">
+    <button class="fButton">Join!</button>
+  </div>
+</div>
+<div class="footerRightMenu">
+  <h1 class="fMenuTitle">Follow Us</h1>
+  <div class="fIcons">
+    <img src="./images/facebook.png" alt="" class="fIcon">
+    <img src="./images/twitter.png" alt="" class="fIcon">
+    <img src="./images/instagram.png" alt="" class="fIcon">
+    <img src="./images/whatsapp.png" alt="" class="fIcon">
+  </div>
+</div>
+<div class="footerRightMenu">
+  <span class="copyright">By: SARIM HUSSAIN <br> All rights reserved. 2022.</span>
+</div>
+</div>
+`;
